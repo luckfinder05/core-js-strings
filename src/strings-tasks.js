c6a372b2e8453ce5989da8faf3d0fb79fa1384b5
Vladislav Novitskiy <no-vlad@yandex.ru>
2023-11-16 17:14:37 +0300

    remove unary ++ operator

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -354,7 +354,7 @@ function findLongestWord(sentence) {
   const workString = sentence.replace(/[^a-z\sA-Z]/g, '');
   const words = workString.split(' ');
   let longestWord = words[0];
-  for (let i = 1; i < words.length; i++) {
+  for (let i = 1; i < words.length; i += 1) {
     if (words[i].length > longestWord.length) {
       longestWord = words[i];
     }
