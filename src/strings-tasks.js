757988b19098c0c170f43e39cc337f3962869686
Vladislav Novitskiy <no-vlad@yandex.ru>
2023-11-16 15:38:05 +0300

    countVowels complete

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -297,8 +297,9 @@ function containsSubstring(str, substring) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'];
+  return [...str].filter((item) => vowels.includes(item)).length;
 }
 
 /**
